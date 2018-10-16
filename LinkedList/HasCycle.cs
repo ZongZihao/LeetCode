7d2511d7101f139b12439dcc1db4bad617e2d03e
wppzy <wppzy@DESKTOP-AHE5HCB>
2018-10-16 09:51:42 +0800

    暴力法判断环形链表

---
+++ LinkedList/HasCycle.cs
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    //暴力法
+    //空间复杂度 O(n)
+    //时间复杂度 O(n²)
+    public bool HasCycle(ListNode head) {
+        //将遍历过的节点添加到数组中
+        var list = new List<ListNode>();
+        var cur = head;
+        while(cur != null){
+            list.Add(cur);
+            if(list.Contains(cur.next)) return true;
+            cur = cur.next;
+        }
+        return false;
+    }
+}
