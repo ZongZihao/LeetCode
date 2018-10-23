70ddb57df75344651ad9052b4fe7fb47e47292c4
zongzihao <915637831@qq.com>
2018-10-23 22:09:33 +0800

    快排思想找K

--- Sort/FindKthElement.cs
+++ Sort/FindKthElement.cs
@@ -17,4 +17,47 @@ public class Solution {
         }
         return nums[k-1];
     }
+
+    //依据快排思想
+    public int FindKthLargest2(int[] nums, int k) {
+
+        return Find(nums, 0, nums.Length - 1, k);
+        
+    }
+    
+    public int Find(int[] nums, int left, int right, int k){
+        
+        //分区返回轴心点
+        int pivot = Partition(nums, left, right);
+        
+        if(pivot + 1 == k){
+            return nums[pivot];
+        }else if(pivot + 1 < k){
+            return Find(nums, pivot + 1, right, k);
+        }else{
+            return Find(nums, left, pivot - 1, k);
+        }
+    }
+    
+    public int Partition(int[] nums, int left, int right){
+        
+        //选取pivot
+        int pivot = right;
+        //快慢两指针,区分已处理区间
+        int slow = left;
+        int fast = left;
+        for(; fast <= right; fast++){
+            //倒叙排序
+            if(nums[fast] >= nums[pivot]){
+                //交换
+                int tmp = nums[slow];
+                nums[slow] = nums[fast];
+                nums[fast] = tmp;
+                //已处理区间+1
+                slow++;
+            }
+        }
+        return slow - 1;
+        
+    }
 }
